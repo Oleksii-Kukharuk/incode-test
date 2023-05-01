646a018028b8bad74983929f57ad016756f82bcd
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2023-05-01 20:33:12 +0300

    f

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,8 @@
+export default function Form() {
+  return (
+    <form>
+      <input type="input" placeholder="Enter repo URL" />
+      <button type="submit"> Load issues</button>
+    </form>
+  );
+}
